f3358aebc71ed53c4ce9aafcc5a131fe05d754cd
dante <sunchao.0129@163.com>
2018-03-21 22:44:27 +0800

    修改页面格式

--- src/components/sysmgr/Syslog.vue
+++ src/components/sysmgr/Syslog.vue
@@ -1,27 +1,27 @@
 <template>
     <div id="sysLog">
-			<el-form ref="syslogForm" :model="query" :inline="true" class="searchForm" size="mini">
-				<el-form-item label="帐号">
-                    <el-input placeholder="帐号" v-model="query.account"></el-input>
-				</el-form-item>
-                <el-form-item label="IP">
-                    <el-input placeholder="ip" v-model="query.ip"></el-input>
-				</el-form-item>
-                <el-form-item label="访问时间">
-                    <el-date-picker
-                        v-model="query.daterange" 
-                        type="daterange"
-                        range-separator="至"
-                        start-placeholder="开始日期"
-                        end-placeholder="结束日期"
-                        value-format="yyyy-MM-dd">
-                    </el-date-picker>
-                </el-form-item>
-                </el-col>
-				<el-form-item>
-					<el-button type="primary" icon="el-icon-search" @click="search">搜索</el-button>
-				</el-form-item>
-			</el-form>
+        <el-form ref="syslogForm" :model="query" :inline="true" class="searchForm" size="mini">
+            <el-form-item label="帐号">
+                <el-input placeholder="帐号" v-model="query.account"></el-input>
+            </el-form-item>
+            <el-form-item label="IP">
+                <el-input placeholder="ip" v-model="query.ip"></el-input>
+            </el-form-item>
+            <el-form-item label="访问时间">
+                <el-date-picker
+                    v-model="query.daterange" 
+                    type="daterange"
+                    range-separator="至"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期"
+                    value-format="yyyy-MM-dd">
+                </el-date-picker>
+            </el-form-item>
+            </el-col>
+            <el-form-item>
+                <el-button type="primary" icon="el-icon-search" @click="search">搜索</el-button>
+            </el-form-item>
+        </el-form>
         <spirit-table
             ref="syslogTable"
             :table="table" 
